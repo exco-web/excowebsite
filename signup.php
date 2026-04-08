26b69031cfe6b71813d088d5bebde802f281a2a1
Dimitar <exco.website@gmail.com>
2026-04-08 11:13:16 +0100

    small fixes

--- signup.php
+++ signup.php
@@ -112,6 +112,10 @@ if ($_SERVER['REQUEST_METHOD'] == "POST"){
             const phoneInput = window.intlTelInput(phoneInputField, {
                 utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
             });
+
+            phoneInputField.closest("form").addEventListener("submit", function () {
+                phoneInputField.value = phoneInput.getNumber();
+            });
         </script>
     </body>
 </html>
